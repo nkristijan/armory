b730391172c5152bdb19232028edb2a950da9166
Neven Kristijan <neven.kristijan@gmail.com>
2012-12-27 16:23:34 +0100

    New files from last commit.

---
+++ html/admin_html_top.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" type="text/css" href="armory.css">
+	<title>Oružarstvo - <?php echo $title;?></title>
+	</head>
+	
+	<body>
+		<div id="global">
+		<div id="header">
+			<label id="header-text">Oružarstvo</label>
+			<label id="header-logout"><?php include 'scripts/name.php';?></label>
+		<div>
+			<table id="menu">
+				<tr><td><a href="user_home.php">Posudbe</a></td>
+				<td style="width:130px;"><a href="transaction.php">Prijenos opreme</a></td>
+				<td style="color:red"><a href="items.php">Administracija</a></td></tr>
+			</table>
+		</div>
+		</div>
+		<div id="content">
+			<div id="content-center">
